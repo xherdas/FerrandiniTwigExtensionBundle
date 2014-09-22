acf98c38c731e66b8e766874c8f33ac047cf516d
Simon Caron <simon@distantia.ca>
2014-09-22 14:13:14 -0400

    Tweak bundle;

--- Twig/FerrandiniExtension.php
+++ Twig/FerrandiniExtension.php
@@ -6,55 +6,17 @@
  */
 namespace Ferrandini\Bundle\TwigExtensionBundle\Twig;
 
-use Symfony\Component\Routing\Router;
-use Symfony\Component\HttpFoundation\ParameterBag;
-
 class FerrandiniExtension extends \Twig_Extension
-{
-    /**
-     * @var \Symfony\Component\Routing\Router
-     */
-    protected $router;
-
-    /**
-     * @param \Symfony\Component\Routing\Router $router
-     */
-    public function __construct(Router $router)
-    {
-        $this->router = $router;
-    }
-
+{   
     /**
      * @return array
      */
     public function getFunctions()
     {
         return array(
-            'route_located' => new \Twig_Function_Method($this, 'routeLocated'),
             'convert_time' => new \Twig_Function_Method($this, 'convertTime'),
         );
-    }
-
-    /**
-     * @param $route_name
-     * @param \Symfony\Component\HttpFoundation\ParameterBag $parameters
-     * @param $locale
-     * @param bool $absolute
-     *
-     * @return string
-     */
-    public function routeLocated($route_name, ParameterBag $parameters, $locale, $absolute = false)
-    {
-        $parameters->set('_locale', $locale);
-
-        foreach ($parameters as $key => $value) {
-            if (mb_strpos($key, '_') === 0 && $key != '_locale') {
-                $parameters->remove($key);
-            }
-        }
-
-        return $this->router->generate($route_name, $parameters->all(), $absolute);
-    }
+    }   
 
     /**
      * Convert time in seconds to unit
